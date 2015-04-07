8c29c3e39f148c676be926b5952969be161f4519
Arpit Mohan <mohanarpit@users.noreply.github.com>
2015-04-07 12:31:41 +0530

    Adding more dependencies for virtualbox

--- install_script.sh
+++ install_script.sh
@@ -60,7 +60,7 @@ mkdir -p ~/projects/vagrant
 
 cd ~/projects/vagrant (
 		wget http://files.vagrantup.com/packages/a40522f5fabccb9ddabad03d836e120ff5d14093/vagrant_1.3.5_x86_64.deb
-		sudo apt-get install virtualbox -y
+		sudo apt-get install virtualbox dkms virtualbox-guest-additions-iso -y
 		sudo dpkg -i vagrant_1.3.5_x86_64.deb
 		rm vagrant_1.3.5_x86_64.deb
 	)
